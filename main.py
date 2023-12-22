5791b5416bd923849494ae436aca118edd45b73d
ClaudioMontenegro <claudiocmjbio@gmail.com>
2023-12-22 15:09:59 -0300

    Second commit: program improved with exceptions, search button, and JSON file.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 import pandas as pd
@@ -8,14 +9,37 @@ import pyperclip
 
 LIGHT_RED = "#E4BE9E"
 DARK_PURPLE = "#27213C"
-LIGHT = "#dedaed"
 ENT_BG = "#dedaed"
 FONT_TEXT = "Open Sans"
 FONT_SIZE = 10
 my_passwords = pd.read_csv("my-passwords/my_passwords.csv", sep=',')
 
+# ---------------------------- SEARCHER PASSWORD -------------------------------- #
+
+
+def search_pass():
+    site = web_ent.get().title()
+    try:
+        with open("my-passwords/my_sites.json", "r") as datafile:
+            # Reading the JSON file
+            data = json.load(datafile)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message="There are no sites register yet!")
+    else:
+        if site in data:
+            the_pass = data[site]["password"]
+            the_email = data[site]["email"]
+            messagebox.showinfo(title=f'{site}', message=f"Email: {the_email}\nPassword: {the_pass}")
+            email_ent.delete(0, 'end')
+            pass_ent.delete(0, 'end')
+            email_ent.insert(0, the_email)
+            pass_ent.insert(0, the_pass)
+        else:
+            messagebox.showinfo(title='Error', message="No details for this website exists.")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def pass_gen():
     pass_ent.delete(0, 'end')
     password = []
@@ -31,12 +55,13 @@ def pass_gen():
     pyperclip.copy(password)
     pass_ent.insert(0, password)
 
-
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save_pass():
     # Information required
     global my_passwords
-    website = web_ent.get()
+    website = web_ent.get().title()
     email = email_ent.get()
     passw = pass_ent.get()
     date = datetime.now()
@@ -52,16 +77,39 @@ def save_pass():
             # Concat the information into a base dataframe
             new_record = pd.DataFrame([{'sites': website,
                                         'emails': email,
-                                       'passwords': passw,
+                                        'passwords': passw,
                                         'date': date_now}])
             my_passwords = pd.concat([my_passwords, new_record], ignore_index=True)
 
             # Save as .csv file
             my_passwords.to_csv("my-passwords/my_passwords.csv", sep=',', index=False)
+            # Save as .json
+            my_sites = {
+                website:
+                    {'email': email,
+                     'password': passw,
+                     'date': date_now}}
+            try:
+                with open("my-passwords/my_sites.json", "r") as datafile:
+                    # Reading the JSON file
+                    data = json.load(datafile)
+            except FileNotFoundError:
+                # Write a new JSON file
+                with open("my-passwords/my_sites.json", "w") as datafile:
+                    json.dump(my_sites, datafile, indent=4)
+            else:
+                # Save the alterations into the JSON file
 
-    # Delete the information on the entries
-    web_ent.delete(0, 'end')
-    pass_ent.delete(0, 'end')
+                # Update the JSON file
+                data.update(my_sites)
+
+                with open("my-passwords/my_sites.json", "w") as datafile:
+                    json.dump(data, datafile, indent=4)
+            finally:
+                # Delete the information on the entries
+                web_ent.delete(0, 'end')
+                pass_ent.delete(0, 'end')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -78,39 +126,47 @@ canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
 # Labels
-web_lb = Label(text="Website:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=LIGHT,)
+web_lb = Label(text="Website:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=ENT_BG, )
 web_lb.grid(column=0, row=1)
-email_lb = Label(text="Email/Username:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=LIGHT,)
+email_lb = Label(text="Email/Username:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=ENT_BG, )
 email_lb.grid(column=0, row=2)
-pass_lb = Label(text="Password:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=LIGHT,)
+pass_lb = Label(text="Password:", font=(FONT_TEXT, FONT_SIZE, "bold"), bg=DARK_PURPLE, fg=ENT_BG, )
 pass_lb.grid(column=0, row=3)
 
 # Buttons
 gen = PhotoImage(file="buttons/gen_bt3.png")
-add = PhotoImage(file="buttons/add2.png")
-gen_pass_lb = Button(image=gen,
+add = PhotoImage(file="buttons/add.png")
+sear = PhotoImage(file="buttons/search.png")
+gen_pass_bt = Button(image=gen,
                      font=(FONT_TEXT, FONT_SIZE),
                      bg=DARK_PURPLE,
                      command=pass_gen,
                      border=0,
                      borderwidth=0)
-gen_pass_lb.config(activebackground=DARK_PURPLE)
-gen_pass_lb.grid(column=2, row=3, sticky='w')
-add_lb = Button(image=add, font=(FONT_TEXT, FONT_SIZE),
+gen_pass_bt.config(activebackground=DARK_PURPLE)
+gen_pass_bt.grid(column=2, row=3, sticky='w')
+add_bt = Button(image=add, font=(FONT_TEXT, FONT_SIZE),
                 width=36,
                 bg=DARK_PURPLE,
                 command=save_pass,
                 borderwidth=0,
                 border=0)
-add_lb.config(activebackground=DARK_PURPLE)
-add_lb.grid(column=1, row=4, columnspan=2, sticky='ew')
+add_bt.config(activebackground=DARK_PURPLE)
+add_bt.grid(column=1, row=4, columnspan=2, sticky='we')
+search_bt = Button(image=sear,
+                   bg=DARK_PURPLE,
+                   border=0,
+                   borderwidth=0,
+                   command=search_pass)
+search_bt.config(activebackground=DARK_PURPLE)
+search_bt.grid(column=2, row=1, sticky='we')
 
 # Entries
 web_ent = Entry(width=35, bg=ENT_BG)
-web_ent.grid(column=1, row=1, columnspan=2, sticky='we')
+web_ent.grid(column=1, row=1, columnspan=1, sticky='we')
 web_ent.focus()
-email_ent = Entry(width=35, bg=ENT_BG)
-email_ent.grid(column=1, row=2, columnspan=2, sticky='we')
+email_ent = Entry(width=54, bg=ENT_BG)
+email_ent.grid(column=1, row=2, columnspan=2, sticky='w')
 email_ent.insert(0, "youraddress@mail.com")
 pass_ent = Entry(width=21, bg=ENT_BG)
 pass_ent.grid(column=1, row=3, sticky='we')
